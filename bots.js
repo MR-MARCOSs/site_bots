48e2b0f824dea7b574e24abb553f999afc7d3f84
MR-MARCOSs <marcosdudu001@gmail.com>
2024-12-19 23:58:27 -0300

    bots list

---
+++ bots.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        name: "Goobe",
+        descricao: "Goobe the duck",
+        endpoint: "...",
+    },
+
+    {
+        name: "GPT-4o",
+        descricao: "ChatGPT 4o",
+        endpoint: "...",
+    },
+
+    {
+        name: "Gemini 1.0 Pro",
+        descricao: "Google Gemini 1.0 Pro",
+        endpoint: "...",
+    }
+
+];
