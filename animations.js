9dd5153bb8d80d9f7bdc06f44f543bfa1605ce6d
Marcos Eduardo <marcosdudu001@gmail.com>
2025-01-02 17:18:16 -0300

    chat media querie started

--- animations.js
+++ animations.js
@@ -1,3 +1,52 @@
+function toggleChatbots() {
+    const chatbots = document.querySelector('.chatbots');
+    const chatConversation = document.querySelector('.chat-conversation');
+    const inputContainer = document.querySelector('.input-container');
+    const arrow = document.querySelector('.arrow');
+    const arrowIcon = document.querySelector('.arrow i');
+    
+    // Alterna a visibilidade do chatbots
+    chatbots.classList.toggle('show'); 
+
+    // Verifica se a seta está visível ou não
+    if (arrowIcon.classList.contains('fa-arrow-left')) {
+        // Se estiver visível, remove as classes de movimento reverso
+        if (chatbots.classList.contains('move-left-reverse')) {
+            chatbots.classList.remove('move-left-reverse');
+            chatConversation.classList.remove('move-center-reverse');
+            inputContainer.classList.remove('move-input-center-reverse');
+            arrow.classList.remove('arrow-move-reverse');
+        }
+
+        // Adiciona as classes de movimento
+        chatbots.classList.add('move-left');
+        chatConversation.classList.add('move-center');
+        inputContainer.classList.add('move-input-center');
+        arrow.classList.add('arrow-move');
+        arrowIcon.classList.remove('fa-arrow-left');
+        arrowIcon.classList.add('fa-arrow-right');
+    } else {
+        // Se não estiver visível, remove as classes de movimento
+        chatbots.classList.remove('move-left');
+        chatConversation.classList.remove('move-center');
+        inputContainer.classList.remove('move-input-center');
+        arrow.classList.remove('arrow-move');
+        arrowIcon.classList.remove('fa-arrow-right');
+        arrowIcon.classList.add('fa-arrow-left');
+
+        // Adiciona as classes de movimento reverso
+        chatbots.classList.add('move-left-reverse');
+        chatConversation.classList.add('move-center-reverse');
+        inputContainer.classList.add('move-input-center-reverse');
+        arrow.classList.add('arrow-move-reverse');
+    }
+}
+
+// Associa o clique na seta para exibir ou ocultar os chatbots
+document.querySelector('.arrow').addEventListener('click', toggleChatbots);
+
+
+
 function animation() {
     const arrow = document.querySelector('.arrow');
     const arrowIcon = document.querySelector('.arrow i');
