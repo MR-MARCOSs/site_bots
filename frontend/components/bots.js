5b135ca1467e62036d70155bc69f6f1473bdb7e4
Marcos Eduardo <marcosdudu001@gmail.com>
2025-01-03 14:17:15 -0300

    frontend structure

--- frontend/components/bots.js
+++ frontend/components/bots.js
@@ -1,21 +1,21 @@
 let dados = [
     {
         name: "Goobe",
-        img: "assets/goobe.png",
+        img: "../assets/goobe.png",
         descricao: "Goobe the duck",
         endpoint: "...",
     },
 
     {
         name: "GPT-4o",
-        img: "assets/gpt.jpg",
+        img: "../assets/gpt.jpg",
         descricao: "ChatGPT 4o",
         endpoint: "...",
     },
 
     {
         name: "Gemini 1.0 Pro",
-        img: "assets/gemini.png",
+        img: "../assets/gemini.png",
         descricao: "Google Gemini 1.0 Pro",
         endpoint: "...",
     }
