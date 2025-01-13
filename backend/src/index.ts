6d6080aba8e3e2c8c5a083c8598cbbf65835f464
Marcos Eduardo <marcosdudu001@gmail.com>
2025-01-13 18:05:35 -0300

    bots loading in front end

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,22 @@
 import express from 'express'
 import { AppDataSource } from './data-source'
 import routes from './routes'
-
+const cors = require('cors');
+  // Importando o pacote cors
 
 AppDataSource.initialize().then(() => {
     const app = express()
+
+    // Habilitar o CORS para todas as origen
+    app.use(cors())
 
     app.use(express.json())
 
     app.use(routes)
 
-    return app.listen(process.env.PORT)
+    // Definir a porta
+    const port = process.env.PORT || 3000
+    return app.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}`)
+    })
 })
